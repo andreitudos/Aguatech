29841757faa92f7a54c28b41ddd3216cbb07911b
Andrei Tudos <14925@formandos.cinel.pt>
2019-04-07 19:33:57 +0100

    Products Controller API

---
+++ Aguatech.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿namespace Aguatech.Web.Controllers.API
+{
+    using Aguatech.Web.Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
